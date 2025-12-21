f28e7507acbbb70d8bd138283e92fa4c5f5fbf37
VijayTeja-10 <vijayteja8000@gmail.com>
2025-12-21 14:16:57 +0530

    Registerapi

--- BackendDRF/api/serializers.py
+++ BackendDRF/api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class RegisterSerializer(serializers.ModelSerializer):
     #write_only to say hide password at get req's
     #style for representing password field in html of restframework endpoint
-    password=serializers.CharField(write_only=True,style={'input_type':'password'})
+    password=serializers.CharField(write_only=True,min_len=8,style={'input_type':'password'})
     class Meta:
         model=User
         fields=['username','email','password']
