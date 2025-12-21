13de3e72bd0fb062b82ede28b99d797754d29a8c
VijayTeja-10 <vijayteja8000@gmail.com>
2025-12-21 14:15:41 +0530

    RegisterApi

--- BackendDRF/api/serializers.py
+++ BackendDRF/api/serializers.py
@@ -3,12 +3,22 @@ from rest_framework import serializers
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    #write_only to say hide password at get req's
+    #style for representing password field in html of restframework endpoint
+    password=serializers.CharField(write_only=True,style={'input_type':'password'})
     class Meta:
         model=User
         fields=['username','email','password']
 
+    def create(self, validated_data):
+        #create => donot hashes the password
+        #create_user => hashes password automatically
+        #user=User.objects.create_user(**validated_data) => **validated_data automatically takes fields and all fields should be in model&Metaclass
+        user=User.objects.create_user(validated_data['username'],validated_data['email'],validated_data['password'])
+        return user
+
 class LoginSerializer(serializers.ModelSerializer):
-    password=serializers.CharField()
+    # password=serializers.CharField(write_only=True)
     class Meta:
         model=User
         fields=['username','password']
--- BackendDRF/api/views.py
+++ BackendDRF/api/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import LoginSerializer,RegisterSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
-
 class Login(generics.ListAPIView):
     queryset=User.objects.all()
-    serializer_class=LoginSerializer
+    serializer_class=LoginSerializer    
 
 
 class Register(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class=RegisterSerializer
+    permission_classes=[AllowAny]
