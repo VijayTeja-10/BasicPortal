7da2875f8545f257fe1784ad32d8be3e711b701f
VijayTeja-10 <vijayteja8000@gmail.com>
2025-12-24 17:03:32 +0530

    Final

--- FrontendReact/src/components/Main.jsx
+++ FrontendReact/src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
                 <p>Your Demo Portal page is ready. It introduces the portal as a safe space for exploring features, testing workflows, and learning through interactive modules. The content highlights key features like a user-friendly interface, secure access, and interactive demos, plus a simple "Getting Started" guide and benefits for new users.</p>
                 <div>
                   {isLoggedIn ? (
-                  <Button text='Explore Now' cls={'btn-info'} url='/dashboard' />
+                  <Button text='Explore Now!' cls={'btn-info'} url='/dashboard' />
                   ):(
                   <>
                   <Button text={'Login'} cls={'btn-info'} url='/login' />
